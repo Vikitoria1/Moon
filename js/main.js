aad84953a65ee688eaf52fec9fd9dd84e6f44feb
Viktoria Grushko <grushko13vika@gmail.com>
2024-07-04 17:39:51 +0300

    changes 6

--- js/main.js
+++ js/main.js
@@ -4,3 +4,30 @@ const inner = document.querySelector('.header__inner');
 btn.addEventListener('click', ()=>{
     inner.classList.toggle('menu-open');
 });
+
+$(function(){
+    $('.team__list').slick({
+        arrows: false,
+        infinity: false,
+        slidesToShow: 4,
+        slidesToScroll: 4,
+  responsive: [
+    {
+      breakpoint: 940,
+      settings: {
+        arrows: false,
+        infinity: true,
+        slidesToShow: 2,
+        slidesToScroll: 2,
+      }
+    },
+    {
+      breakpoint: 430,
+      settings: {
+        slidesToShow: 1.5,
+        slidesToScroll: 1.5
+      }
+    }
+  ]
+});
+});
