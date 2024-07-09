ac71dad1269fa847fa52c20a97e725c6c7208003
Viktoria Grushko <grushko13vika@gmail.com>
2024-07-09 18:23:01 +0300

    changes 8

--- js/main.js
+++ js/main.js
@@ -1,60 +1,76 @@
-const btn = document.querySelector('.menu-btn');
-const inner = document.querySelector('.header__inner');
+const btn = document.querySelector(".menu-btn");
+const inner = document.querySelector(".header__inner");
 
-btn.addEventListener('click', ()=>{
-    inner.classList.toggle('menu-open');
-});
-
-$(function(){
-    $('.catalog__list').slick({
-        arrows: false,
-        infinity: false,
-        slidesToShow: 4,
-        slidesToScroll: 4,
-  responsive: [
-    {
-      breakpoint: 940,
-      settings: {
-        arrows: false,
-        infinity: true,
-        slidesToShow: 2,
-        slidesToScroll: 2,
-      }
-    },
-    {
-      breakpoint: 430,
-      settings: {
-        slidesToShow: 1.5,
-        slidesToScroll: 1.5
-      }
-    }
-  ]
-});
+btn.addEventListener("click", () => {
+  inner.classList.toggle("menu-open");
 });
 
-$(function(){
-    $('.team__list', '.catalog__list').slick({
-        arrows: false,
-        infinity: false,
-        slidesToShow: 4,
-        slidesToScroll: 4,
-  responsive: [
-    {
-      breakpoint: 940,
-      settings: {
-        arrows: false,
-        infinity: true,
-        slidesToShow: 2,
-        slidesToScroll: 2,
-      }
-    },
-    {
-      breakpoint: 430,
-      settings: {
-        slidesToShow: 1.5,
-        slidesToScroll: 1.5
-      }
-    }
-  ]
+$(document).ready(function () {
+  $(".catalog__list").slick({
+    arrows: false,
+    infinity: false,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    variableWidth: true,
+    responsive: [
+      {
+        breakpoint: 1142,
+        settings: {
+          infinity: true,
+          slidesToShow: 3,
+          slidesToScroll: 1,
+        },
+      },
+      {
+        breakpoint: 940,
+        settings: {
+          infinity: true,
+          slidesToShow: 2,
+          slidesToScroll: 1,
+        },
+      },
+      // {
+      //   breakpoint: 430,
+      //   settings: {
+      //     slidesToShow: 1.5,
+      //     slidesToScroll: 1,
+      //   },
+      // },
+    ],
+  });
 });
+
+$(document).ready(function () {
+  $(".team__list").slick({
+    arrows: false,
+    infinity: false,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    variableWidth: true,
+    responsive: [
+      {
+        breakpoint: 1142,
+        settings: {
+          infinity: true,
+          slidesToShow: 3,
+          slidesToScroll: 1,
+        },
+      },
+      {
+        breakpoint: 940,
+        settings: {
+          infinity: true,
+          slidesToShow: 2,
+          slidesToScroll: 1,
+        },
+      },
+      // {
+      //   breakpoint: 430,
+      //   settings: {
+      //     slidesToShow: 1.5,
+      //     slidesToScroll: 1.5,
+      //   },
+      // },
+    ],
+  });
 });
